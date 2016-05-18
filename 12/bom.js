24e97b9495a18ca454b7f9e7ae1150427ee031b6
lccccc <984129111@qq.com>
2016-05-18 12:39:54 +0800

    12练习

---
+++ 12/bom.js
@@ -0,0 +1,24 @@
+
+
+var person = {
+	name:'MR . haha',
+	gender:'male',
+	age:18,
+	sing: function(){console.log('正在笑')}
+}
+//console.log(person.name)
+//person.sing()
+   for(var i in console){console.log(i + ":" + console[i]);}
+   console.log(typeof person);
+//布尔类型
+console.log(typeof false);
+console.log(typeof 'falese');
+//数字类型
+console.log(typeof 100);
+//字符串类型
+console.log(typeof person.name);
+//定义数组
+var sites=['baidu.com','souhu.com','163.com'];
+
+console.log(sites[0]);
+console.log(typeof sites)
